85b4ae6a7705c427d00bd2017962a8f78dfba9d0
Gobind Puniani <gobindpuniani@gmail.com>
2022-03-01 12:56:15 -0800

    Created struct and tried to scrape NASA page

--- scrape.go
+++ scrape.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/extensions"
 )
+
+type scrapedData struct {
+	Mission string
+	// Description string
+}
+
+// type jsonData struct {
+// 	Mission string
+// 	// Description string
+// }
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
 	// Instantiate default collector
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.CacheDir("./.cache"),
+	)
+	extensions.RandomUserAgent(c)
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	// Mission Title
+	c.OnHTML("h3", func(e *colly.HTMLElement) {
+		fmt.Print("Hi")
+        nasaUpcomingMission := &scrapedData {
+			Mission: e.Text,
+		}
+		
 
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+        fmt.Printf("Link found: %q -> %s\n", e.Text, nasaUpcomingMission.Mission)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +46,29 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+
+	
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+	
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+	
+	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
+		fmt.Println("First column of a table row:", e.Text)
+	})
+	
+	c.OnXML("//h1", func(e *colly.XMLElement) {
+		fmt.Println(e.Text)
+	})
+	
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+
+	// Start scraping on NASA's upcoming launches page
+	c.Visit("https://www.nasa.gov/launchschedule/")
 }
