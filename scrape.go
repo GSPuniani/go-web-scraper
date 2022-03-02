3caff2f7fe0380c24a4fd2a62ec6e213fd3048c0
Gobind Puniani <gobindpuniani@gmail.com>
2022-03-02 14:11:18 -0800

    Finished scraper

--- scrape.go
+++ scrape.go
@@ -11,13 +11,8 @@ import (
 
 type scrapedData struct {
 	Mission string
-	// Description string
 }
 
-// type jsonData struct {
-// 	Mission string
-// 	// Description string
-// }
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -28,25 +23,20 @@ func main() {
 	)
 	extensions.RandomUserAgent(c)
 
+	upcomingMissions := make([]scrapedData, 0)
+	var missions []string
+
 
 	// Mission Title
 	c.OnHTML("h5", func(e *colly.HTMLElement) {
-        upcomingMissions := &scrapedData {
-			Mission: strings.TrimSpace(e.Text),
-		}
+        // upcomingMissions := &scrapedData {
+		// 	Mission: strings.TrimSpace(e.Text),
+		// }
 
-		// Print mission
-        fmt.Printf("Upcoming mission: %s\n", upcomingMissions.Mission)
-
-		// Print mission in JSON format
-		missionsJSON, _ := json.Marshal(upcomingMissions)
-		fmt.Println(string(missionsJSON))
-
-		// Save mission to JSON file
-		file, _ := json.MarshalIndent(upcomingMissions, "", " ")
-		_ = ioutil.WriteFile("output.json", file, 0644)
+		missions = append(missions, strings.TrimSpace(e.Text))
 	})
 
+	
 	c.OnError(func(_ *colly.Response, err error) {
 		fmt.Println("Something went wrong:", err)
 	})
@@ -70,4 +60,21 @@ func main() {
 
 	// Start scraping on NASA's upcoming launches page
 	c.Visit("https://nextspaceflight.com/launches/")
+
+
+	for i := 0; i < len(missions); i++ {
+		missionData := scrapedData{Mission: missions[i]}
+		upcomingMissions = append(upcomingMissions, missionData)
+	}
+
+	// Print mission
+	fmt.Printf("\nUpcoming missions: %s\n", upcomingMissions)
+
+	// Print mission in JSON format
+	missionsJSON, _ := json.Marshal(upcomingMissions)
+	fmt.Printf("\nUpcoming missions in JSON: %s\n", string(missionsJSON))
+
+	// Save mission to JSON file
+	file, _ := json.MarshalIndent(upcomingMissions, "", " ")
+	_ = ioutil.WriteFile("output.json", file, 0644)
 }
